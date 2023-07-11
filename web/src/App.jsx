f1a0c36cb495b50089ce7f524c624ac9c871502e
nikita-skobov <nikita@skobov.io>
2023-07-11 17:43:08 -0600

    web will reload and fetch new wasm

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,74 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useCallback } from 'react'
 import './App.css'
 import { load_wasm } from './wasm'
+
+function getWsUri() {
+  var loc = window.location, new_uri;
+  if (loc.protocol === "https:") {
+      new_uri = "wss:";
+  } else {
+      new_uri = "ws:";
+  }
+  new_uri += "//" + loc.host;
+  new_uri += loc.pathname + "ws";
+  return new_uri;
+}
+
+function AppWs({ cb }) {
+  const ws = useRef(null);
+  const uri = getWsUri();
+
+  useEffect(() => {
+      ws.current = new WebSocket(uri);
+      ws.current.onopen = () => console.log("ws opened");
+      ws.current.onclose = () => console.log("ws closed");
+
+      const wsCurrent = ws.current;
+
+      return () => {
+          wsCurrent.close();
+      };
+  }, []);
+
+  useEffect(() => {
+    if (!ws.current) return;
+
+    setInterval(() => {
+      ws.current.send('test');
+    }, 300)
+
+    ws.current.onmessage = (e) => {
+      if (e.data === "true") {
+        cb();
+      }
+    };
+  }, [ws]);
+}
 
 function App() {
   const canvasRef = useRef(null);
   let [wasmObj, setWasmObj] = useState(null);
-  const [count, setCount] = useState(0)
+  const [isInitialLoad, setInitialLoad] = useState(true);
+  // const [shouldFetch, setShouldFetch] = useState(false);
+
+  // const con = new WebSocket(new_uri);
+  // console.log(con);
+  // con.addEventListener("message", (event) => {
+  //   console.log("Message from server ", event.data);
+  //   let s = event.data;
+  //   if (s === "true") {
+      
+  //   }
+  // });
+
 
   useEffect(() => {
-    if (count === 0) { return }
-    if (wasmObj) {
-      wasmObj.cancel_animation();
-      wasmObj = null;
+    if (isInitialLoad) {
+      setInitialLoad(false);
+    } else {
+      return
     }
+    console.log('doing initial load')
     let ignore = false;
     const getWasm = async () => {
       const data = await load_wasm(`current.wasm`);
@@ -27,17 +83,11 @@ function App() {
     return () => {
       ignore = true;
     };
-  }, [count])
+  }, [wasmObj, isInitialLoad, setInitialLoad, setWasmObj])
 
   useEffect(() => {
-    console.log(`calling use effect with: count=${count}`)
-    console.log(wasmObj);
-    if (count === 0) { return }
     if (!canvasRef || !canvasRef.current || !wasmObj) { return }
-
-    console.log('got em!');
-    // console.log(wasmObj);
-    // console.log(canvasRef);
+    console.log('got new wasm!');
     const canvasCurr = canvasRef.current;
     const glCurr = canvasCurr.getContext('webgl2');
     wasmObj.set_canvas(canvasCurr);
@@ -50,19 +100,24 @@ function App() {
         console.warn(e);
       }
     }, 1)
-  }, [count, canvasRef, wasmObj])
+  }, [canvasRef, wasmObj])
+
+  const cb = useCallback(() => {
+    console.log("IN CB. getting new wasm");
+    if (wasmObj) {
+      wasmObj.cancel_animation();
+      wasmObj = null;
+    }
+    const getWasm = async () => {
+      const data = await load_wasm(`current.wasm`);
+      setWasmObj(data);
+    }
+    getWasm();
+  }, [wasmObj, setWasmObj]);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count === 0 ? 'None loaded yet' : `eg${count}.wasm`}
-        </button>
-        <p>
-          Click button to load new wasm
-        </p>
-      </div>
+      <AppWs cb={cb}/>
       <canvas ref={canvasRef} id="glcanvas" tabIndex='1' />
     </>
   )
